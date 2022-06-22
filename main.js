09db21e7fb1cc3375db116a6d00b76e0cee179f3
shofukuhara <sho.fukuhara0213@gmail.com>
2022-06-23 00:42:57 +0900

    study: 2022.6.23_0:43

--- main.js
+++ main.js
@@ -1,31 +1,9 @@
-// const postal = {
-//   postalCode: "123-4567",
-//   checkPastalCode() {
-//     // replacedにpostalCodeのメソッドの中身を置き換えた物を代入
-//     const replaced = this.postalCode.replace("-", "").length;
-//     // lengthにreplacedの数を代入
-//     // const length = replaced.length;
-//     if (replaced === 7) {
-//       return true;
-//     }
-//     return false;
-//   },
-// };
+function hello(callback) {
+  console.log(callback);
+  console.log("hello " + callback());
+}
 
-// console.log(postal.checkPastalCode());
 
-const person = {
-  name: "本田",
-  age: 30,
-
-  getName() {
-    console.log(this.name);
-    return this;
-  },
-  getAge() {
-    console.log(this.age);
-    return this;
-  },
-};
-
-person.getName().getAge();
+hello(function () {
+  return "sho";
+});
