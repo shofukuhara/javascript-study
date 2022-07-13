ea47d3820033dec8829e83d0f924a58d7ee4d2f2
shofukuhara <sho.fukuhara0213@gmail.com>
2022-07-13 23:55:51 +0900

    study:背景色が左から右に消え文字出現(未完成)

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-let num = 0;
-let tar = 200;
+// window.addEventListener("scroll", function () {
+//   if (this.scrollY < 600) {
+//     let num = 0;
+//     let tar = 200;
+//     setInterval(function () {
+//       if (num <= tar) {
+//         document.querySelector(".number").innerText = num;
+//         num++;
+//       }
+//     }, 20);
+//   }
+// });
+const bg = document.querySelector(".bgappearTrigger");
+bg.style.removeProperty("color");
 
-setInterval(function () {
-  if (num <= tar) {
-    document.querySelector(".number").innerText = num;
-    num++;
-  }
-}, 20);
+// const bg = getComputedStyle(document.querySelector(".bgappearTrigger"),"::before").top;
+
+// console.log(bg);
+window.addEventListener("scroll", function () {});
